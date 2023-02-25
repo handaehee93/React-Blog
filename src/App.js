5487f302bbc204a9fb454cde262efde52c2b6e62
handaehee93 <hdh8990@naver.com>
2023-02-25 14:35:28 +0900

    TodoList컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import NavBar from './components/NavBar';
-
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Home />
     </div>
   );
 }
